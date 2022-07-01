e775e99ca702f25f0f24fd8233f0c082ba957fb3
VPB-winhobbit <vpbphone@gmail.com>
2022-07-01 18:21:54 -0500

    Cleanup of CRA files

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Focus-Links (typeScript edition)</h1>
       </header>
     </div>
   );
